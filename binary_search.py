17e50e88bf02adabb8b6666a68f90f83362c2a7b
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2022-03-25 18:54:29 -0700

    Code Bug fixes

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,24 @@
 def binary_search(sequence, item):
-    begin_index = 0
-    end_index = len(sequence) - 1
+    if len(sequence) > 0:
+        begin_index = 0
+        end_index = len(sequence) - 1
 
-    while begin_index < end_index:
-        midpoint = begin_index + (end_index - begin_index) // 2
-        middle_value = sequence[midpoint]
+        while begin_index < end_index:
+            midpoint = begin_index + (end_index - begin_index) // 2
+            middle_value = sequence[midpoint]
 
-        if item == middle_value:
-            return midpoint
-        
-        elif item < middle_value:
-            end_index = midpoint - 1
+            if item == middle_value:
+                return midpoint
+            
+            elif item < middle_value:
+                end_index = midpoint - 1
 
-        else:
-            begin_index = midpoint + 1
-    return None
+            else:
+                begin_index = midpoint + 1
+        return None
+    else:
+        print("Please enter a valid sequence")
 
-x = [3,4,12,34,45,66,89,90,101,112,134,145]
-print(binary_search(x, 45)) 
+#x = [1,2,3,22,33,45,69,75,88,90,103,112,128,130,148]
+x = []
+print(binary_search(x, 112)) 
