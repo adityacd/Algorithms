02fe05fb983645885421695fcb38f253e4fa980c
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2022-03-28 15:13:03 -0700

    Code for Insertion Sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,2 +1,13 @@
 def insertion_sort(sequence):
-    
+    initial_index = range(1, len(sequence))
+
+    for i in initial_index:
+        values_to_sort = sequence[i]
+
+        while sequence[i-1] > values_to_sort and i>0:
+            sequence[i-1], sequence[i] = sequence[i], sequence[i-1]
+            i = i-1
+    return sequence
+
+x = [2,4,1,5,8,22,0,3]
+print(insertion_sort(x))
